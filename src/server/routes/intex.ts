e39fab23a4db66c162becdbec2fbb069d8411bcd
marcolaleal <marcolaleal@gmail.com>
2022-12-06 23:14:23 -0300

    feat: CRUD cidades init

--- src/server/routes/intex.ts
+++ src/server/routes/intex.ts
@@ -10,10 +10,12 @@ router.get('/',(_,res) =>{
     return res.send('Ola Dev! A API esta rodando');
 });
 
-router.post('/cidades',  
-    CidadesController.createValidation,
-    CidadesController.create
-);
+router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
+router.post('/cidades', CidadesController.createValidation, CidadesController.create);
+router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById);
+router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
+router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById);
+
 
 
 
