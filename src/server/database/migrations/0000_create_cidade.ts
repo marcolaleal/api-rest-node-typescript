58ecc7bf1a3590d02fe1c88a66645390e69f9902
marcolaleal <marcolaleal@gmail.com>
2023-02-13 22:22:14 -0300

    feat: model e migrations de usuarios completas

--- src/server/database/migrations/0000_create_cidade.ts
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -1,4 +1,4 @@
-import knex, { Knex } from 'knex';
+import { Knex } from 'knex';
 import { ETableNames } from '../ETableNames';
 
 
--- src/server/database/models/index.ts
+++ src/server/database/models/index.ts
@@ -1,3 +1,5 @@
 export * from './Cidade';
 export * from './Pessoa';
+export * from './Usuario';
+
 
